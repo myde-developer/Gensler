c999e9ff8fd2f88ef4dd3ea28961b80b25d06bdb
YourName <myde1323@gmail.com>
2025-12-14 20:37:16 +0100

    Initial Commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -297,7 +297,7 @@ export default function Home() {
     <>
       <Nav />
       
-      <main className="pt-10">
+      <main className="pt-5">
         <DesignForecast />
         <section className="py-20 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
